b71b55367022fb1ed9f536aecbedf6ad0a297104
Danielle Jasper <dnj547@live.com>
2019-03-11 16:29:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,8 +27,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holidays, supplies|
     holidays[supplies] << supply
-    binding.pry
   end
+  binding.pry
 end
 
 
