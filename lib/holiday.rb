727a230d6dc9202008bde779b8f57872f62ce63a
Danielle Jasper <dnj547@live.com>
2019-03-11 16:46:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash
-  binding.pry
+  holiday_hash.each do |seasons, holidays|
+    puts seasons
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
