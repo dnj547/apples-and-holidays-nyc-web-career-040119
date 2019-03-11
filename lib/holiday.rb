e1af131b68f511769c83182b30cf10dd38015531
Danielle Jasper <dnj547@live.com>
2019-03-11 16:32:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,8 +26,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holidays, supplies|
+        binding.pry
     holidays[supplies] << supply
-    binding.pry
+
   end
 end
 
